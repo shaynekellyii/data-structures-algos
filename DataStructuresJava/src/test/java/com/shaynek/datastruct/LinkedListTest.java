b4ab9974898aae0b6e25a6565e6430bfb6045359
Shayne Kelly II <skshayne@gmail.com>
2019-04-11 01:28:53 -0700

    Add support for Java, Kotlin, and Dart testing with Travis CI (#1)
    * Add travis.yml file
    
    * Update travis.yml
    
    * Add pom mvn files
    
    * Update travis.yml to cd to correct directories
    
    * Update travis.yml to cd to correct directories again
    
    * Fix dart test command and add kotlin directory to test script

--- DataStructuresJava/src/test/java/com/shaynek/datastruct/LinkedListTest.java
+++ DataStructuresJava/src/test/java/com/shaynek/datastruct/LinkedListTest.java
@@ -1,8 +1,13 @@
 package com.shaynek.datastruct;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class LinkedListTest {
+    @Test
+    public void stubTest() {
+        assert true;
+    }
 }
