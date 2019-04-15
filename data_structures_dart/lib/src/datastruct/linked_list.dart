cb5fe547e2925ef04e89437541af84a94da0b1a0
Shayne Kelly II <skshayne@gmail.com>
2019-04-14 23:29:16 -0700

    Add 100% coverage for dart linkedlist

--- data_structures_dart/lib/src/datastruct/linked_list.dart
+++ data_structures_dart/lib/src/datastruct/linked_list.dart
@@ -1,23 +1,31 @@
 class LinkedList<T> {
-  _Node<T> head;
+  LinkedList() {
+    _length = 0;
+  }
+
+  _Node<T> _head;
+  int _length;
+
+  T get first => _head?.data;
+
+  T get last => _lastNode()?.data;
+
+  int get length => _length;
 
   void append(T data) {
     final newNode = _Node(data);
-    if (head != null) {
-      head = newNode;
+    _length++;
+    if (_head == null) {
+      _head = newNode;
     } else {
-
+      _lastNode().next = newNode;
     }
   }
 
-  T first() => head?.data;
-
-  T last() => _lastNode().data;
-
   _Node<T> _lastNode() {
-    if (head == null) return null;
+    if (_head == null) return null;
 
-    _Node current = head;
+    _Node current = _head;
     while (current.next != null) {
       current = current.next;
     }
@@ -26,8 +34,17 @@ class LinkedList<T> {
 
   @override
   String toString() {
-    if (head == null) return "Empty linked list";
-    return super.toString();
+    if (_head == null) return "Empty linked list";
+
+    _Node current = _head;
+    StringBuffer buffer = StringBuffer();
+
+    while (current != null) {
+      buffer.write('${current.data.toString()} -> ');
+      current = current.next;
+    }
+    buffer.write('null');
+    return buffer.toString();
   }
 }
 
--- data_structures_dart/test/linked_list_test.dart
+++ data_structures_dart/test/linked_list_test.dart
@@ -6,7 +6,35 @@ void main() {
     expect(LinkedList(), isNotNull);
   });
 
-  test('Empty list', () {
-    expect(LinkedList().toString(), "Empty linked list");
+  test('Append test', () {
+    final list = LinkedList<int>();
+    expect(list.length, 0);
+
+    list.append(1);
+    expect(list.length, 1);
+    expect(list.first, 1);
+    expect(list.last, 1);
+
+    list.append(2);
+    expect(list.length, 2);
+    expect(list.first, 1);
+    expect(list.last, 2);
+  });
+
+  test('First and last element of empty list', () {
+    final list = LinkedList<String>();
+    expect(list.first, isNull);
+    expect(list.last, isNull);
+  });
+
+  test('toString() test', () {
+    final list = LinkedList<int>();
+    expect(list.toString(), 'Empty linked list');
+
+    list.append(1);
+    expect(list.toString(), '1 -> null');
+
+    list.append(5);
+    expect(list.toString(), '1 -> 5 -> null');
   });
 }
