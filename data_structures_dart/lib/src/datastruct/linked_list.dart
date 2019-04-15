2109e9fa4112b8d749787c174cebe9d1744e4651
Shayne Kelly II <skshayne@gmail.com>
2019-04-14 19:37:23 -0700

    Add code coverage utility for dart code

--- data_structures_dart/lib/src/datastruct/linked_list.dart
+++ data_structures_dart/lib/src/datastruct/linked_list.dart
@@ -1,1 +1,39 @@
-class LinkedList {}
+class LinkedList<T> {
+  _Node<T> head;
+
+  void append(T data) {
+    final newNode = _Node(data);
+    if (head != null) {
+      head = newNode;
+    } else {
+
+    }
+  }
+
+  T first() => head?.data;
+
+  T last() => _lastNode().data;
+
+  _Node<T> _lastNode() {
+    if (head == null) return null;
+
+    _Node current = head;
+    while (current.next != null) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  @override
+  String toString() {
+    if (head == null) return "Empty linked list";
+    return super.toString();
+  }
+}
+
+class _Node<T> {
+  _Node(this.data);
+
+  T data;
+  _Node<T> next;
+}
--- data_structures_dart/test/linked_list_test.dart
+++ data_structures_dart/test/linked_list_test.dart
@@ -1,5 +1,12 @@
+import 'package:data_structures/src/datastruct/linked_list.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Basic test', () {});
+  test('Basic test', () {
+    expect(LinkedList(), isNotNull);
+  });
+
+  test('Empty list', () {
+    expect(LinkedList().toString(), "Empty linked list");
+  });
 }
